09da1830004a97528ac1e564b16e790c69d2db2d
Dionis <Usual.routine.geek@gmail.com>
2026-05-22 18:42:38 +0500

    feat: update index page and configuration for Russian language support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+  server: {
+    host: '0.0.0.0',
+    port: 4321
+  }
 });
